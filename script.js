edc5a8a7363856116047ea3f4ff94292bdea9c9c
Darrian DeVaughnn <devondevaughnn@gmail.com>
2020-09-10 15:21:57 -0700

    added psuedocode

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+var number = []
+for (var i = 0; charLength.length; i++)
 function generatePassword();
 //TODO: Create generatePassword function that returns password. all code should live here
 // TODO: create prompts to ask length of the password, save it as a VAR
@@ -17,5 +20,10 @@ function generatePassword();
 //TODO: add validations to make sure atleast one type of char is selected
 //TODO: After prompt is done, password should create
 //TODO: Create arrays that have all numbers, all upper, all special, all lower case
+//TODO: Create new var that holds all the chosen char
+//TODO: create an if statement that savves the chosen char from our confirms, to a new collection
+//TODO: Select specific numvber of char randomly from our final collection based off of the users answer to how long the length of the password
+//I can use Math.floor(Math.random()) to get random number
+//for loop to the length of the selected char var (for var = i=0; i=< "charLength prompt"; i++){logic goes here}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
