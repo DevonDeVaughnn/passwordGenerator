e607ee92549f0e55e6c9a43342522a439c6c789f
Darrian DeVaughnn <devondevaughnn@gmail.com>
2020-09-10 15:20:22 -0700

    added more psuedocode

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+function generatePassword();
+//TODO: Create generatePassword function that returns password. all code should live here
+// TODO: create prompts to ask length of the password, save it as a VAR
+//TODO: Create confirm that asks if they want lower, upper, numeric and/or special
+//TODO: Length should be no more than 128 char, and no less than 8 char
+//TODO: add validations to make sure atleast one type of char is selected
+//TODO: After prompt is done, password should create
+//TODO: Create arrays that have all numbers, all upper, all special, all lower case
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
